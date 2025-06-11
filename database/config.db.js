10ade00073ca54200d684fc34b29755b46991a84
alfredo-vargas <alfvargas@alumnos.uai.cl>
2025-06-11 18:16:49 -0400

    Add database configuration and web scraping functionality

---
+++ database/config.db.js
@@ -0,0 +1,17 @@
+const pg = require('pg');
+const { Pool } = pg;
+const dotenv = require('dotenv');
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+
+module.exports = pool;
--- web/scrapping.js
+++ web/scrapping.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const jsdom = require('jsdom');
+const pool = require('../database/config.db');
 
 async function scrapeWebsite() {
     try {
@@ -32,8 +33,8 @@ async function scrapeWebsite() {
                 const item = {
                     productName: element.querySelector('.grid-view-item__title.product-card__title')?.textContent.trim(),
                     productPrice: element.querySelector('.price.price--listing.price--sale') ?
-                        element.querySelector('.price-item.price-item--sale').textContent.trim() :
-                        element.querySelector('.price-item.price-item--regular').textContent.trim(),
+                        Number(element.querySelector('.price-item.price-item--sale').textContent.trim().replace('$', '').replace('.', '')) :
+                        Number(element.querySelector('.price-item.price-item--regular').textContent.trim().replace('$', '').replace('.', '')),
                     productImage: element.querySelector('.grid-view-item__image-wrapper.product-card__image-wrapper.js').getElementsByTagName('div')[0].getElementsByTagName('img')[0].getAttribute('data-src') ?
                         'https:' + element.querySelector('.grid-view-item__image-wrapper.product-card__image-wrapper.js').getElementsByTagName('div')[0].getElementsByTagName('img')[0].getAttribute('data-src').split('?')[0].replace('{width}', '600') :
                         'https:' + element.querySelector('.grid-view-item__image-wrapper.product-card__image-wrapper.js').getElementsByTagName('div')[0].getElementsByTagName('img')[0].getAttribute('data-srcset').replace('{width}', '600').split('?')[0],
@@ -64,6 +65,43 @@ async function scrapeWebsite() {
             }
         }
 
+        const createQuery = `
+            CREATE TABLE IF NOT EXISTS products (
+                id SERIAL PRIMARY KEY,
+                product_name VARCHAR(255),
+                product_price NUMERIC,
+                product_image TEXT,
+                product_link TEXT
+            );`;
+
+        await pool.query(createQuery);
+
+        const selectQuery = 'SELECT * FROM products;';
+        const existingProducts = await pool.query(selectQuery);
+
+        const existingProductsInBBDD = existingProducts.rows.map(product => product);
+
+        existingProductsInBBDD.forEach(async (product) => {
+            const index = products.findIndex(p => p.productName.includes(product.product_name))
+            if (index !== -1) {
+                products.splice(index, 1);
+            }
+        });
+
+        if (products.length > 0) {
+            const insertQuery = `
+                INSERT INTO products (product_name, product_price, product_image, product_link)
+                VALUES ($1, $2, $3, $4)`;
+
+            for (const product of products) {
+                await pool.query(insertQuery, [
+                    product.productName,
+                    product.productPrice,
+                    product.productImage,
+                    product.productLink
+                ]);
+            }
+        }
         await browser.close();
     } catch (error) {
         console.error('Error during web scraping:', error);
